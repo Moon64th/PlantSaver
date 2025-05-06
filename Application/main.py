617514a68cd7e13a823d5705e03b20679f8e706b
Leandro <leandro@Leandros-MacBook-Pro.local>
2025-05-06 08:54:03 +0200

    Added items

---
+++ Application/main.py
@@ -0,0 +1,5 @@
+sf = input("enter Software to use (Discord/Email/Whatsapp)")
+BOT_TOKEN = "MTM2OTE5OTU1OTk4MDM1MTUwOA.G8ieci.O1tWp3dz1RS0dKz-I7IUA9fyaAhFurgDXuivCc"
+
+if sf == "Discord":
+    print()
